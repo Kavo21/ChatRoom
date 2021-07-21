01aa6c1c65e40db065184e1b84e65125230cd71b
Kavo <34242547+Kavo21@users.noreply.github.com>
2021-07-21 15:15:09 +0800

    Update test.lua

--- test/test.lua
+++ test/test.lua
@@ -1,17 +1,27 @@
 return {
-    resource_definitions = function()
-        return {
-            settings = function(resource_object)
-                resource_object:set_data_value("turboredis_timeout", 15)
-                resource_object:set_data_value("default_property_size", 500)
-                resource_object:set_data_value("redfish_redis_key_prefix", {
-                        string.format("Redfish:%s:%s:%s", "UpdateService", "FirmwareInventory")
-                    })
-                resource_object:set_data_value("redfish_resource_uri_pattern", {
-                        string.format("^%s/UpdateService/FirmwareInventory/([^/]+)$", config.SERVICE_PREFIX),
-                    })
-                resource_object:set_data_value("redfish_resource_superordinate", "UpdateService")
-                resource_object:set_data_value("redfish_resource_subordinate", nil)
-            end,
-        }
-    end,
+    property_definitions = function()
+                return {
+                    endpoint_settings = {
+                        ["ServiceEnabled"] = function(property_object)
+                            property_object:set_data_value("redis_lua_access_prehook", {})
+                            property_object:set_data_value("redis_lua_access_posthook", {})
+                            property_object:set_data_value("turboredis_access_prehook", {})
+                            property_object:set_data_value("turboredis_access_posthook", {})
+                            property_object:set_data_value("readonly", false)
+                            property_object:set_data_value("property_type", {"boolean", "nil"})
+                            property_object:set_data_value("property_size", 500)
+                        end,
+                        ["Status"] = {
+                            ["Status"] = function(property_object)
+                                property_object:set_data_value("redis_lua_access_prehook", {})
+                                property_object:set_data_value("redis_lua_access_posthook", {})
+                                property_object:set_data_value("turboredis_access_prehook", {})
+                                property_object:set_data_value("turboredis_access_posthook", {})
+                                property_object:set_data_value("readonly", false)
+                                property_object:set_data_value("property_type", {"boolean", "nil"})
+                                property_object:set_data_value("property_size", 500)
+                            end,
+                       },
+                 },
+            }
+     end,
