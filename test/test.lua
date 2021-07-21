f09502a8d53e37ed18f9d070b68d780bff5f0bc6
Kavo <34242547+Kavo21@users.noreply.github.com>
2021-07-21 18:46:10 +0800

    Update test.lua

--- test/test.lua
+++ test/test.lua
@@ -1,27 +1,16 @@
-return {
-    property_definitions = function()
+return function()
+        return {
+            resource_definitions = function()
                 return {
-                    endpoint_settings = {
-                        ["ServiceEnabled"] = function(property_object)
-                            property_object:set_data_value("redis_lua_access_prehook", {})
-                            property_object:set_data_value("redis_lua_access_posthook", {})
-                            property_object:set_data_value("turboredis_access_prehook", {})
-                            property_object:set_data_value("turboredis_access_posthook", {})
-                            property_object:set_data_value("readonly", false)
-                            property_object:set_data_value("property_type", {"boolean", "nil"})
-                            property_object:set_data_value("property_size", 500)
-                        end,
-                        ["Status"] = {
-                            ["Status"] = function(property_object)
-                                property_object:set_data_value("redis_lua_access_prehook", {})
-                                property_object:set_data_value("redis_lua_access_posthook", {})
-                                property_object:set_data_value("turboredis_access_prehook", {})
-                                property_object:set_data_value("turboredis_access_posthook", {})
-                                property_object:set_data_value("readonly", false)
-                                property_object:set_data_value("property_type", {"boolean", "nil"})
-                                property_object:set_data_value("property_size", 500)
-                            end,
-                       },
-                 },
             }
-     end,
+           end,
+            property_definitions = function()
+                return {
+            }
+           end,
+           operations = function()
+                return {
+            }
+           end,
+        }
+end
