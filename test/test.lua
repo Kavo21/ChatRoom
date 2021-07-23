191a66246ea64bb7077ebb5e55c4c856052e590c
Kavo <34242547+Kavo21@users.noreply.github.com>
2021-07-23 10:40:21 +0800

    Update test.lua

--- test/test.lua
+++ test/test.lua
@@ -1,8 +1,10 @@
     operations = function()
         return {
-            GET = function(operations_params)end,
-            PATCH = function(operations_params)end,
-            POST = function(operations_params)end,
-            DELETE = function(operations_params)end,
+            GET = function(operations_params)
+                local db_access_clients = operations_params.db_access_clients
+                local response_body = operations_params.response_body
+                local response_data = db_access_clients:redis_lua_access()
+                utils.copy_table_endpoint(response_body, response_data, {"Status"})
+            end,
         }
     end,
