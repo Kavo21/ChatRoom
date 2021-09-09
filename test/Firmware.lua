93bd7e72c0b627bc3f75ec0bcd2ac06ac42d5cbc
Kavo <34242547+Kavo21@users.noreply.github.com>
2021-09-09 18:24:54 +0800

    Update Firmware.lua

--- test/Firmware.lua
+++ test/Firmware.lua
@@ -60,10 +60,7 @@ return {
                 local response_body = operations_params.response_body
                 local db_access_clients = operations_params.db_access_clients
                 local response_data = db_access_clients:redis_lua_access()
-
                 utils.copy_table_endpoint(response_body, response_data, {"ServiceEnabled"})
-                utils.copy_table_endpoint(response_body, response_data, {"PreferredNtpServer"})
-                utils.copy_table_endpoint(response_body, response_data, {"AlternateNtpServer"})
             end,
             PATCH = function(operations_params)
                 local db_access_clients = operations_params.db_access_clients
