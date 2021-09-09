69ce8270c6802e0fc819519fa354408da528f208
Kavo <34242547+Kavo21@users.noreply.github.com>
2021-09-09 14:43:16 +0800

    Update Firmware.lua

--- test/Firmware.lua
+++ test/Firmware.lua
@@ -1,10 +1,11 @@
 property_definitions = function(definitions_params)
 
         local endpoint_settings = {
-            ["Name"] = function(property_object)
+            ["ServiceEnabled"] = function(property_object)
+                 property_object:set_data_value("redis_lua_access_cache_enabled", false)
                  property_object:set_data_value("redis_lua_access_prehook", {
                         GET = function(prehook_params)
-                            prehook_params.pl:get(string.format("%s:Name", prehook_params.redis_key_prefix))
+                            prehook_params.pl:get(string.format("%s:ServiceEnabled", prehook_params.redis_key_prefix))
                         end,
                     })
                 property_object:set_data_value("redis_lua_access_posthook", {
