568465d23995b60b6a825bd3387b2bd7a68dd2f1
Kavo <34242547+Kavo21@users.noreply.github.com>
2021-09-09 14:04:44 +0800

    Update Firmware.lua

--- test/Firmware.lua
+++ test/Firmware.lua
@@ -1,21 +1,21 @@
-return {
-    resource_definitions = function(definitions_params)
+property_definitions = function(definitions_params)
 
-        local settings = function(resource_object)
-            resource_object:set_data_value("turboredis_timeout", 15) -- sec
-            resource_object:set_data_value("redfish_redis_key_prefix", {
-                    string.format("Redfish:%s", "Managers")
-                })
-            resource_object:set_data_value("redfish_resource_uri_pattern", {
-                    string.format("^%s/Managers$", config.SERVICE_PREFIX),
-                })
-            resource_object:set_data_value("redfish_resource_superordinate", "ServiceRoot")
-            resource_object:set_data_value("redfish_resource_subordinate", {
-                    "ManagersInstance",
-                })
-        end
-
+        local endpoint_settings = {
+            ["Name"] = function(property_object)
+                 property_object:set_data_value("redis_lua_access_prehook", {
+                        GET = function(prehook_params)
+                            prehook_params.pl:get(string.format("%s:Name", prehook_params.redis_key_prefix))
+                        end,
+                    })
+                property_object:set_data_value("redis_lua_access_posthook", {
+                        GET = function(posthook_params)
+                            return posthook_params.pl_replies[1]
+                        end,
+                    })
+            end,
+        }
         return {
-            settings = settings,
+            endpoint_settings = endpoint_settings,
         }
     end,
+
