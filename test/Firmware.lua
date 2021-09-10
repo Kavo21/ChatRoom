2df6a0465051b43db8fbf9ccf8c031b06c629778
Kavo <34242547+Kavo21@users.noreply.github.com>
2021-09-10 10:11:25 +0800

    Update Firmware.lua

--- test/Firmware.lua
+++ test/Firmware.lua
@@ -62,10 +62,6 @@ return {
                 local response_data = db_access_clients:redis_lua_access()
                 utils.copy_table_endpoint(response_body, response_data, {"ServiceEnabled"})
             end,
-            PATCH = function(operations_params)
-                local db_access_clients = operations_params.db_access_clients
-                local response_data = db_access_clients:turboredis_access()
-            end,
         }
 
         return {
